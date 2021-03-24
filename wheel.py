8d40df585a8dd078450532ef314840d3e831eba4
Aishwaryak2998 <63080568+Aishwaryak2998@users.noreply.github.com>
2021-03-24 17:45:53 +0530

    Update wheel.py
    Wheels move in the forward and backward direction

--- wheel.py
+++ wheel.py
@@ -1,3 +1,69 @@
+////24th March 2021////
+
+import sys
+import time
+import RPi.GPIO as GPIO
+
+mode=GPIO.getmode()
+
+GPIO.cleanup()
+
+Forward=26
+fwd = 19
+backward=20
+bkd = 16
+sleeptime=1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Forward, GPIO.OUT)
+GPIO.setup(fwd, GPIO.OUT)
+GPIO.setup(backward, GPIO.OUT)
+GPIO.setup(bkd, GPIO.OUT)
+
+def aage(x):
+    
+	GPIO.output(fwd, GPIO.HIGH)
+	print("Moving Forward")
+	time.sleep(x)
+	GPIO.output(fwd, GPIO.LOW)
+	
+    
+
+def forward(x):
+	GPIO.output(Forward, GPIO.HIGH)
+	GPIO.output(fwd, GPIO.HIGH)
+	print("Moving Forward")
+	time.sleep(x)
+	GPIO.output(Forward, GPIO.LOW)
+	GPIO.output(fwd, GPIO.LOW)
+	
+
+def reverse(x):
+	GPIO.output(backward, GPIO.HIGH)
+	GPIO.output(bkd, GPIO.HIGH)
+	print("Moving Backward")
+	time.sleep(x)
+	GPIO.output(backward, GPIO.LOW)
+	GPIO.output(bkd, GPIO.LOW)
+	
+def peeche(x):
+    GPIO.output(bkd, GPIO.HIGH)
+    print("moving bak")
+    time.sleep(x)
+    GPIO.output(bkd, GPIO.LOW)
+
+    
+
+while (1):
+
+	forward(5)
+	#aage(5)
+
+	reverse(5)
+	#peeche(5)
+	GPIO.cleanup()
+////////// 24th MArch 2021
+
 
 import sys
 import time
