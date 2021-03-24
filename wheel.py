3d166aef62edc6911767c680e7d1a33b62841f2c
Aishwaryak2998 <63080568+Aishwaryak2998@users.noreply.github.com>
2021-03-24 17:59:39 +0530

    Update wheel.py
    LEft, right, front and back

--- wheel.py
+++ wheel.py
@@ -1,5 +1,3 @@
-////24th March 2021////
-
 import sys
 import time
 import RPi.GPIO as GPIO
@@ -38,6 +36,21 @@ def forward(x):
 	GPIO.output(fwd, GPIO.LOW)
 	
 
+def left(x):
+    GPIO.output(Forward, GPIO.HIGH)
+    print(" moving left ")
+    time.sleep(x)
+    GPIO.output(Forward, GPIO.LOW)
+    
+def right(x):
+    GPIO.output(fwd, GPIO.HIGH)
+    print("Moving right")
+    time.sleep(x)
+    GPIO.output(fwd, GPIO.LOW)
+    
+    
+    
+
 def reverse(x):
 	GPIO.output(backward, GPIO.HIGH)
 	GPIO.output(bkd, GPIO.HIGH)
@@ -51,92 +64,17 @@ def peeche(x):
     print("moving bak")
     time.sleep(x)
     GPIO.output(bkd, GPIO.LOW)
+    
 
     
 
 while (1):
 
-	forward(5)
+	forward(1)
+	left(0.5)
+	right(0.5)
 	#aage(5)
 
-	reverse(5)
+	reverse(1)
 	#peeche(5)
 	GPIO.cleanup()
-////////// 24th MArch 2021
-
-
-import sys
-import time
-import RPi.GPIO as GPIO
-
-mode= GPIO.getmode()
-
-#GPIO.cleanup()
-
-Forward=26
-Backward=21
-sleeptime=1
-
-fwd = 17
-bck = 18    
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(Forward, GPIO.OUT)
-GPIO.setup(Backward, GPIO.OUT)
-
-GPIO.setup(fwd, GPIO.OUT)
-GPIO.setup(bck, GPIO.OUT)
-
-
-GPIO.setwarnings(False)
-
-def forward(x):
-	GPIO.output(Forward, GPIO.HIGH) 
-	GPIO.output(fwd, GPIO.HIGH) 
-	
-	print("Moving Forward")
-	time.sleep(x)
-	GPIO.output(Forward, GPIO.LOW)
-	GPIO.output(fwd, GPIO.LOW)
-
-
-def reverse(x):
-	GPIO.output(Backward, GPIO.HIGH)
-	GPIO.output(bck, GPIO.HIGH)
-	
-	print("Moving Backward")
-	time.sleep(x)
-	GPIO.output(Backward, GPIO.LOW)
-	GPIO.output(bck, GPIO.LOW)
-
-
-def left(x):
-	#GPIO.output(fwd, GPIO.HIGH)
-	
-	GPIO.output(Forward, GPIO.HIGH)
-	print('left')
-	time.sleep(x)
-	#GPIO.output(fwd, GPIO.LOW)
-	GPIO.output(Forward, GPIO.LOW)  # bck is left wheel
-	
-def right(x):
-	
-	GPIO.output(fwd, GPIO.HIGH)
-	#GPIO.output(Backward, GPIO.HIGH)
-	print('right')
-	time.sleep(x)
-	
-	GPIO.output(fwd , GPIO.LOW)
-	#GPIO.output(Backward, GPIO.LOW)
-
-while (1):
-
-	#forward(5)
-
-	reverse(5)
-	#left(5)
-	#right(5)
-	GPIO.cleanup()
-
-
